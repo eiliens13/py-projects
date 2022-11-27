b033cbefdd5315f3d57830248c0f9e6d9c5010f7
eiliens13 <daily.p.tito@gmail.com>
2022-11-26 21:14:47 -0500

    YALA

---
+++ historias locas/historias_locas.py
@@ -0,0 +1,20 @@
+# Concatenar cadenas de caracters.
+#Aprende a programar con _________________.
+
+# organización = "freeCodeCamp"
+
+# print ("Aprende a programar con " + organización)
+# print("aprende a programar con {}".format(organización))
+# print(f"aPrende a programar con {organización}")
+
+# Mad Libs (Historias Locas)
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+
+madlib = f"! Programar es tan {adj}! Siempre me emociona proque me encanta {verbo1} problemas. Aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural}"
+
+print(madlib)
